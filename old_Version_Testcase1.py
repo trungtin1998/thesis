26296da43f8acd4955b9ee253c79f41cf3e57872
trungtin1998 <phamtrungtintf1512@gmail.com>
2020-06-12 16:19:33 +0700

    Creating DSL query sent request each 5 minutes

--- old_Version_Testcase1.py
+++ old_Version_Testcase1.py
@@ -1,13 +1,11 @@
-#!/usr/bin/python
+#!/usr/bin/env python3
 #-*- coding: utf-8 -*-
 import requests
 import json
-import datetime
 
-# Global Variables
 BASEURL = "http://192.168.255.200:9200/_search"
-QUERY =json.dumps({
-    "query": {
+QUERY = json.dumps({
+        "query": {
         "bool": {
             "filter": [{
                 "match_all": {}
@@ -22,8 +20,8 @@ QUERY =json.dumps({
             }, {
                 "range": {
                     "@timestamp": {
-                        "gte": "AAAAAAAAAAAA",
-                        "lte": "BBBBBBBBBBBB",
+                        "gte": "2020-05-10T15:59:56.888Z",
+                        "lte": "2020-06-09T15:59:56.888Z",
                         "format": "strict_date_optional_time"
                     }
                 }
@@ -31,40 +29,23 @@ QUERY =json.dumps({
         }
     }
 })
+
 HEADER = {"Content-Type" : "application/json"}
 
-# Get current time and time after 5 minutes
-def getTimestamp():
-    test1 = datetime.datetime.now()
-    test2 = test1 + datetime.timedelta(minutes=5)
-    s1 = test1.strftime("%Y-%m-%dT%H:%M:%S.888Z")
-    s2 = test2.strftime("%Y-%m-%dT%H:%M:%S.888Z")
-    return [s1,s2]
-
-# Send Post Request to ELK
 def TestCase1():
     try:
         # Send Query to ELK
-        l = getTimestamp()
-        tmp = QUERY.replace("AAAAAAAAAAAA", l[0])
-        tmp = tmp.replace("BBBBBBBBBBBB", l[1])
-        response = requests.post(BASEURL, headers = HEADER, data = tmp)
+        response = requests.post(BASEURL, headers = HEADER, data = QUERY)
         return response
     except:
         print("Error when create Post Request")
         exit()
 
-# Print Response as prettyprint Format
-def printResponse(response):
-    parsed = json.loads(response.text)
-    print(json.dumps(parsed, indent=4, sort_keys=True))
-
 if __name__ == "__main__":
     response = TestCase1()
     try:
-        printResponse(response)
+        resp_text = json.loads(response.text)
+        print(resp_text)
     except:
         print("cURL to ELK error")
         resp_text = error
-
-
